7fdf17f78e3d8b84dc1166e64fee105ae326f15f
wikibootup <wikibootup@gmail.com>
2021-04-04 15:03:18 +0900

    Added uvicorn test2

---
+++ uvicorn/example2.py
@@ -0,0 +1,37 @@
+import aiohttp
+import asyncio
+
+
+async def read_body(receive):
+    """
+    Read and return the entire body from an incoming ASGI message.
+    """
+    body = b'sd'
+
+    async with aiohttp.ClientSession() as session:
+        async with session.get('http://python.org') as response:
+            print("Status:", response.status)
+            print("Content-type:", response.headers['content-type'])
+
+            html = await response.text()
+            print("Body:", html[:15], "...")
+
+    return body
+
+
+async def app(scope, receive, send):
+    """
+    Echo the request body back in an HTTP response.
+    """
+    body = await read_body(receive)
+    await send({
+        'type': 'http.response.start',
+        'status': 200,
+        'headers': [
+            [b'content-type', b'text/plain'],
+        ]
+    })
+    await send({
+        'type': 'http.response.body',
+        'body': body,
+    })
